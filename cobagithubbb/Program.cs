68be76cb63e66bcf656b0cb337388708aacceacb
BillyAnthonyB <BillyAnthonyB@github.com>
2020-12-18 00:31:09 +0700

    Add project files.

---
+++ cobagithubbb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cobagithubbb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
